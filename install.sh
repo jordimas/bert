7716dc01100b67dd9925ad1655b7e539fff2537b
Jordi Mas <jmas@softcatala.org>
2020-09-29 21:05:29 +0200

    Fix

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 pip install transformers==3.2.0 # in sync with run_language_modeling
 pip install torch==1.6.0
-pip install tensorboard
+pip install tensorboard==2.3.0
 
 git config --global user.name "Jordi Mas"
 git config --global user.email jmas@softcatala.org

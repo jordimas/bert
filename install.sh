9dcd83f7d075ad3a749e528b4d8e8938807f1502
Jordi Mas <jmas@softcatala.org>
2020-09-29 21:03:55 +0200

    Remote machine

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
 pip install transformers==3.2.0 # in sync with run_language_modeling
 pip install torch==1.6.0
+pip install tensorboard
+
+git config --global user.name "Jordi Mas"
+git config --global user.email jmas@softcatala.org
+git clone https://github.com/jordimas/bert.git
+
+mkdir bert/dataset

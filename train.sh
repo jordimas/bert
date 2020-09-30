de23b5b5ab84109be4036e643f9c30892d021a9b
Jordi Mas <jmas@softcatala.org>
2020-09-30 13:41:54 +0000

    train

--- train.sh
+++ train.sh
@@ -7,19 +7,19 @@ mkdir -p  $WEIGTHS_DIR
 python3 model_config.py
 
 # Arguments documentation: https://huggingface.co/transformers/master/_modules/transformers/training_args.html
-CUDA_LAUNCH_BLOCKING=1 python3 transformers/examples/run_language_modeling.py \
+python3 transformers/examples/run_language_modeling.py \
     --output_dir $WEIGTHS_DIR \
     --model_type roberta \
     --mlm \
     --train_data_file dataset/src-train.txt \
-    --eval_data_file dataset/src-eval.txt \
+    --eval_data_file dataset/src-val.txt \
     --config_name $MODEL_DIR \
     --tokenizer_name $MODEL_DIR \
     --do_train \
     --line_by_line \
     --overwrite_output_dir \
     --do_eval \
-    --block_size 256 \
+    --block_size 128 \
     --learning_rate 1e-4 \
     --num_train_epochs 5 \
     --save_total_limit 2 \

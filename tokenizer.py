bf0c8a308e8abcf69e3631110ee87f189813d3d9
Jordi Mas <jmas@softcatala.org>
2020-10-02 08:16:29 +0200

    Tok

--- tokenizer.py
+++ tokenizer.py
@@ -1,26 +1,18 @@
-#! pip install tokenizers
-import os
-from pathlib import Path
-
 from tokenizers import ByteLevelBPETokenizer
-
-#paths = [str(x) for x in Path("./corpu").glob("**/*.txt")]
 
 # Initialize a tokenizer
 tokenizer = ByteLevelBPETokenizer()
-tokenizer.enable_truncation(max_length=512)
+#tokenizer.enable_truncation(max_length=512)
 
 # Customize training
 
-vocab_size=50000
-paths=['corpus/ca_dedup.txt']
-tokenizer.train(files=paths, vocab_size=vocab_size, min_frequency=2, special_tokens=[
-    "<s>",
-    "<pad>",
-    "</s>",
-    "<unk>",
-    "<mask>",
-])
+vocab_size=50265
+path='corpus/ca_dedup.txt'
+# Customize training
+tokenizer.train(files=path,
+                vocab_size=50265,
+                min_frequency=2,
+                special_tokens=["<s>", "<pad>", "</s>", "<unk>", "<mask>"])
 
 # Save files to disk
 directory = "models/roberta"
@@ -28,4 +20,4 @@ directory = "models/roberta"
 if not os.path.exists(directory):
     os.makedirs(directory)
 
-tokenizer.save(directory, "roberta")
+tokenizer.savee("models/roberta")

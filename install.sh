1f2c1a316fb9ca5bfc5a77e4a112c3f1052c1d9b
Jordi Mas <jmas@softcatala.org>
2020-10-02 08:12:31 +0200

    Install

--- install.sh
+++ install.sh
@@ -2,11 +2,8 @@ mkdir -p dataset
 mkdir -p corpus
 mkdir -p models/roberta
 
-pip install tensorflow==2.1.0
-pip install transformers==2.5.0 # in sync with run_language_modeling
-pip install tokenizers==0.5.0
-pip install torch==1.6.0
-pip install tensorboard==2.1.0
+pip uninstall -y tensorflow
+pip install transformers==2.8.0
 
 git config --global user.name "Jordi Mas"
 git config --global user.email jmas@softcatala.org
@@ -16,4 +13,4 @@ git clone https://github.com/jordimas/bert.git
 
 git clone https://github.com/huggingface/transformers.git
 cd transformers
-git checkout v2.5.0
+git checkout v2.8.0

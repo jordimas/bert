a5a9ec6342c3d14413f24854ab5ed46d973b3f6a
Jordi Mas <jmas@softcatala.org>
2020-10-02 11:34:24 +0200

    Fix

--- train.sh
+++ train.sh
@@ -1,4 +1,3 @@
-#MODEL_DIR="models/roberta"
 OUTPUT_DIR="models/roberta/weights"
 TRAIN_PATH="dataset/src-train.txt"
 EVAL_PATH="dataset/src-val.txt"
@@ -23,7 +22,7 @@ python3 transformers/examples/run_language_modeling.py \
     --num_train_epochs 1 \
     --overwrite_output_dir \
     --block_size 512 \
-    --max_step 25 \
+    --max_step 1000 \
     --warmup_steps 10 \
     --learning_rate 5e-5 \
     --per_gpu_train_batch_size 4 \

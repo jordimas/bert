23d5c70947c221b297aec2b690a4dea9f7b20a03
Jordi Mas <jmas@softcatala.org>
2020-10-02 08:26:38 +0200

    Model train

--- train.sh
+++ train.sh
@@ -1,31 +1,42 @@
-MODEL_DIR="models/roberta"
+#MODEL_DIR="models/roberta"
 OUTPUT_DIR="models/roberta/output"
 WEIGTHS_DIR="models/roberta/weights"
+TRAIN_PATH="dataset/src-train.txt"
+EVAL_PATH="dataset/src-val.txt"
+TOKENIZER_NAME="models/roberta"
+CONFIG_NAME="models/roberta"
 
 mkdir -p  $WEIGTHS_DIR
 
-python3 model_config.py
 
 # Arguments documentation: https://huggingface.co/transformers/master/_modules/transformers/training_args.html
 python3 transformers/examples/run_language_modeling.py \
-    --output_dir $WEIGTHS_DIR \
+    --output_dir $OUTPUT_DIR \
     --model_type roberta \
     --mlm \
-    --train_data_file dataset/src-train.txt \
-    --eval_data_file dataset/src-val.txt \
-    --config_name $MODEL_DIR \
-    --tokenizer_name $MODEL_DIR \
+    --config_name $CONFIG_NAME \
+    --tokenizer_name $TOKENIZER_NAME \
+#    {line_by_line} \
+#    {should_continue} \
+#    {model_name_or_path} \
+    --train_data_file $TRAIN_PATH \
+    --eval_data_file $EVAL_PATH \
     --do_train \
-    --line_by_line \
-    --overwrite_output_dir \
     --do_eval \
-    --block_size 128 \
-    --learning_rate 1e-4 \
-    --num_train_epochs 5 \
-    --save_total_limit 2 \
-    --save_steps 2000 \
-    --logging_steps 500 \
-    --per_gpu_eval_batch_size 32 \
-    --per_gpu_train_batch_size 32 \
-    --evaluate_during_training \
+    --epochs 1 \ # Mine
+#    {do_eval} \
+#    {evaluate_during_training} \
+    --overwrite_output_dir \
+    --block_size 512 \
+    --max_step 25 \
+    --warmup_steps 10 \
+    --learning_rate 5e-5 \
+    --per_gpu_train_batch_size 4 \
+    --gradient_accumulation_steps 4 \
+    --weight_decay 0.01 \
+    --adam_epsilon 1e-6 \
+    --max_grad_norm 100.0 \
+    --save_total_limit 10 \
+    --save_steps 10 \
+    --logging_steps 2 \
     --seed 42

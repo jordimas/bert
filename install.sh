82bd80760c0fb583d5b9f012cb1b1cdcdbcf4631
Jordi Mas <jmas@softcatala.org>
2020-10-05 17:13:05 +0200

    Fixes

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ mkdir -p models/roberta
 
 pip uninstall -y tensorflow
 pip install transformers==2.8.0
+pip install transformers==torch
+pip install tensorbord==2.1.0
+
 
 git config --global user.name "Jordi Mas"
 git config --global user.email jmas@softcatala.org
@@ -11,6 +14,6 @@ git clone https://github.com/jordimas/bert.git
 
 
 
-git clone https://github.com/huggingface/transformers.git
-cd transformers
-git checkout v2.8.0
+#git clone https://github.com/huggingface/transformers.git
+#cd transformers
+#git checkout v2.8.0
